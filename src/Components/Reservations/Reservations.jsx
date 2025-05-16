bb838d1853022a2b4404bfa512876a051e33f4b2
AbdelazizSangare <abdelazizsangare73@gmail.com>
2025-05-16 11:28:54 +0000

    Initial commit

--- src/Components/Reservations/Reservations.jsx
+++ src/Components/Reservations/Reservations.jsx
@@ -63,7 +63,7 @@ const Reservations = () => {
   });
 
   try {
-    const response = await fetch('https://localhost:5000/api/reservations', {
+    const response = await fetch('https://sikany.onrender.com/api/reservations', {
   method: 'POST',
   body: data
 });
